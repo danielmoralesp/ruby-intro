af0f14c16629ad85ad85b3510a6ce13d808ff26b
danielmorales1202@gmail.com <danielmorales1202@gmail.com>
2017-02-10 18:59:44 -0500

    Local variables and basic arithmetical operations

--- challenges/3-local-variables/my_solution.rb
+++ challenges/3-local-variables/my_solution.rb
@@ -1,7 +1,13 @@
 # Local Variables
 
 # Tu solucion abajo:
-
+num1 = 12
+num2 = 24
+sum = num1 + num2
+difference = num1 - num2
+quotient = num1.to_f / num2.to_f
+product = num1 * num2
+modulus = num1.modulo(num2)
 # Pruebas NO LAS BORRES
 
 describe 'num1' do
