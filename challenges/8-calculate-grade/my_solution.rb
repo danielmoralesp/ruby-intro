3ae1b5d8f6d285e05f380bc7c08cd740b93dd7e6
danielmorales1202@gmail.com <danielmorales1202@gmail.com>
2017-02-10 19:37:17 -0500

    Calculate a Letter Grade

--- challenges/8-calculate-grade/my_solution.rb
+++ challenges/8-calculate-grade/my_solution.rb
@@ -2,3 +2,16 @@
 
 # Tu solucion abajo:
 
+def get_grade(integer)
+  if integer >= 90
+    "A"
+  elsif integer >= 80
+    "B"
+  elsif integer >= 70
+    "C"
+  elsif integer >= 60
+    "D"
+  else integer < 60
+    "F"
+  end
+end
