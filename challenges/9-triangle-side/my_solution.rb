c647a7085af48dd84125f970739fdc0dc3e3f49c
danielmorales1202@gmail.com <danielmorales1202@gmail.com>
2017-02-10 20:08:05 -0500

    Valid Triangle

--- challenges/9-triangle-side/my_solution.rb
+++ challenges/9-triangle-side/my_solution.rb
@@ -2,4 +2,15 @@
 
 # Tu solucion abajo:
 
-
+def valid_triangle?(a, b, c)
+  if a >= 0 && b >= 0 && c >= 0
+    true
+  else
+    false
+  end
+  if (a + b > c) && (a + c > b) && (b + c > a)
+    true
+  else
+    false
+  end
+end
