19e193db8744d611b98291e1ca1c69191cf23279
danielmorales1202@gmail.com <danielmorales1202@gmail.com>
2017-02-10 20:43:43 -0500

    Leap Year

--- challenges/10-leap-years/my_solution.rb
+++ challenges/10-leap-years/my_solution.rb
@@ -5,3 +5,14 @@
 
 # Tu solucion abajo:
 
+def leap_year?(years)
+  if (years % 4 == 0) && (years % 100 != 0)
+    true
+  elsif years % 400 == 0
+    true
+  elsif (years % 4 == 0) && (years % 100 == 0) && (years % 400 != 0)
+    false
+  elsif years % 4 != 0
+    false
+  end
+end
