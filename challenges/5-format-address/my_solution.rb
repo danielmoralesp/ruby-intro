23eaea8189c1b6716e355d0335423b30fbfb2a37
danielmorales1202@gmail.com <danielmorales1202@gmail.com>
2017-02-10 19:22:57 -0500

    ejercicio 5 y 6

--- challenges/5-format-address/my_solution.rb
+++ challenges/5-format-address/my_solution.rb
@@ -3,5 +3,5 @@
 # Tu solucion abajo:
 
 def make_address(street, city, state, zip)
-  puts "You live at #{street}, in the beautiful city of #{city}, #{state}. Your zip is #{zip}."
+  return "You live at #{street}, in the beautiful city of #{city}, #{state}. Your zip is #{zip}."
 end
--- challenges/6-math-methods/my_solution.rb
+++ challenges/6-math-methods/my_solution.rb
@@ -2,5 +2,18 @@
 
 # Tu solucion abajo:
 
+def add(a,b)
+  a + b
+end
 
+def subtract(a,b)
+  a - b
+end
 
+def multiply(a,b)
+  a * b
+end
+
+def divide(a,b)
+  a.to_f / b.to_f
+end
